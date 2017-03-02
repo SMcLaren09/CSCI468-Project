6be7e0fa4d07d47e434349a4d1b7a21e9a3a5aea
Darnesey <rdarnell1992@gmail.com>
2017-03-01 19:25:10 -0700

    Working on the submission folder

--- ToSubmit/Submission/Driver.java
+++ ToSubmit/Submission/Driver.java
@@ -3,6 +3,7 @@
 
 import java.io.*;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
 /**
  *
@@ -14,22 +15,36 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+	
+	
         try {
 	    FileReader file = new FileReader(args[0]);
             CharStream stream = new ANTLRInputStream(file);
             junkLexer lexer = new junkLexer(stream);
             Vocabulary vocab = lexer.getVocabulary();
             Token tok = null;
+	    //System.out.println("Attempting to grab tokens");
+	    //System.out.println(lexer.nextToken());
 
-            do {
+	    CommonTokenStream tokens = new CommonTokenStream(lexer);
+	    junkParser parser = new junkParser(tokens);
+	    parser.program();
+	    //System.out.printf("Found %d errors\n", parser.getNumberOfSyntaxErrors());
+	    //System.out.println("Finished parsing.");
+
+	    if (parser.getNumberOfSyntaxErrors() == 0) {
+  		System.out.println("Accepted");
+	    } else {
+		System.out.println("Not accepted");
+	    }
+/**            do {
                 tok = lexer.nextToken();
 		if (tok.getType() == tok.EOF)
 		    break;
 		String tokenType;
                 System.out.println("Token Type: " + vocab.getSymbolicName(tok.getType()) +
                                    "\nValue: " + tok.getText());
-            } while(tok.getType() != tok.EOF);
+            } while(tok.getType() != tok.EOF);*/
         } catch (Exception ex) {
             System.out.println("Failed to do stuff");
         }
