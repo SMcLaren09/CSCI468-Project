3e8e4056bcd562bbd3ee02c6f63099a5be0f8ea3
ryan.darnell <j42p557@bz-cs-cslab13l.msu.montana.edu>
2017-04-29 22:10:08 -0600

    Added expressions behavior

--- src/TinyBuilder.java
+++ src/TinyBuilder.java
@@ -6,6 +6,8 @@ public class TinyBuilder {
 	private SymbolTable table;
 	private ArrayList<String> tiny;
 	private ArrayList<String> vars;
+	private int regNum;
+	private char dataType;
 	//TODO: Symbol table needs to be root Table with the ability to check its children or something... issue with string variables.
 
 	public TinyBuilder(ArrayList<String> ir_list, SymbolTable table) {
@@ -13,6 +15,7 @@ public class TinyBuilder {
 		this.table = table;
 		tiny = new ArrayList<>();
 		vars = new ArrayList<>();
+		regNum = 0; //for storing the temps
 	}
 
 	public void print(boolean vertical) {
@@ -31,6 +34,11 @@ public class TinyBuilder {
 		//let the parsing games begin...
 		for (String opcode : ir_list) {
 			checkForVariable(opcode);
+			String[] ops = opcode.split(" ");
+			char dataType = ops[0].toLowerCase().charAt(ops[0].length() - 1);
+			dataType = dataType == 'f' ? 'r' : dataType;
+
+			//route instruction
 			if (opcode.contains("LABEL")) parseLabel(opcode);
 			else if (opcode.contains("STORE")) parseStore(opcode);
 			else if (opcode.contains("ADD")) parseAdd(opcode);
@@ -55,7 +63,6 @@ public class TinyBuilder {
 			}
 		}
 	}
-
 	private void foundVariable(Symbol var) {
 		String variable = var.getName();
 		vars.add(variable);
@@ -74,32 +81,44 @@ public class TinyBuilder {
 	// STORE[T] op1 op2
 	private void parseStore(String code) {
 		String[] ops = code.split(" ");
+		//we'll see if we need to deal with variable checking...
 		tiny.add(String.format("move %s %s",ops[1],ops[2]));
 	}
 
 	private void parseComp(String code) {
 		String[] ops = code.split(" ");
-
 	}
 
+	// ADD[T] r1 r2 r3
 	private void parseAdd(String code) {
+		code = code.replace("$T","r");
 		String[] ops = code.split(" ");
-
+		tiny.add(String.format("move %s %s",ops[1],ops[3]));
+		tiny.add(String.format("add%c %s %s",dataType,ops[2],ops[3]));
 	}
 
+	// SUB[T] r1 r2 r3
 	private void parseSub(String code) {
+		code = code.replace("$T","r");
 		String[] ops = code.split(" ");
-
+		tiny.add(String.format("move %s %s",ops[1],ops[3]));
+		tiny.add(String.format("sub%c %s %s",dataType,ops[2],ops[3]));
 	}
 
+	// MULT[T] r1 r2 r3
 	private void parseMul(String code) {
+		code = code.replace("$T","r");
 		String[] ops = code.split(" ");
-
+		tiny.add(String.format("move %s %s",ops[1],ops[3]));
+		tiny.add(String.format("mul%c %s %s",dataType,ops[2],ops[3]));
 	}
 
+	// DIV[T] r1 r2 r3
 	private void parseDiv(String code) {
+		code = code.replace("$T","r");
 		String[] ops = code.split(" ");
-
+		tiny.add(String.format("move %s %s",ops[1],ops[3]));
+		tiny.add(String.format("div%c %s %s",dataType,ops[2],ops[3]));
 	}
 
 	// JUMP label#
@@ -111,14 +130,12 @@ public class TinyBuilder {
 	// READ[T] op1
 	private void parseRead(String code) {
 		String[] ops = code.split(" ");
-		char dataType = ops[0].toLowerCase().charAt(ops[0].length() - 1);
 		tiny.add(String.format("sys read%c %s",dataType,ops[1]));
 	}
 
 	// WRITE[T] op1
 	private void parseWrite(String code) {
 		String[] ops = code.split(" ");
-		char dataType = ops[0].toLowerCase().charAt(ops[0].length() - 1);
 		tiny.add(String.format("sys write%c %s",dataType,ops[1]));
 	}
 
